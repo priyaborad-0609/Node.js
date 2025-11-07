41ec0b62ca11c311cb84e67471234370c81a06eb
Priya Borad <pp13120@gmail.com>
2025-11-07 09:16:32 +0530

    7/11/25

---
+++ Day-2 FS/app.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+ fs.writeFileSync("index.html", "<html><head><title>My Web Page</title></head><body><h1>Welcome to My Web Page</h1></body></html>")
+
+ const data = fs.readFileSync("index.html", "utf8")
+ console.log(data)
+
+ fs.appendFileSync("index.html", "<p>This is a paragraph.</p>")
